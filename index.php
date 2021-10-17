6825228a527c13b266486de523e8c609068e3ea1
Bohdan Dobrov <bov.don.94@gmail.com>
2021-10-17 22:44:06 +0300

    refactor: connected the file helpers.php to index.php and added htmlspecialchars function

--- index.php
+++ index.php
@@ -52,28 +52,7 @@ function costs_of_item(float $price): string
     return $price . ' ' . '₽';
 };
 
-/**
- * Подключает шаблон, передает туда данные и возвращает итоговый HTML контент
- * @param string $name Путь к файлу шаблона относительно папки templates
- * @param array $data Ассоциативный массив с данными для шаблона
- * @return string Итоговый HTML
- */
-function include_template($name, array $data = []) {
-    $name = 'templates/' . $name;
-    $result = '';
-
-    if (!is_readable($name)) {
-        return $result;
-    }
-
-    ob_start();
-    extract($data);
-    require $name;
-
-    $result = ob_get_clean();
-
-    return $result;
-}
+require_once('helpers.php');
 
 //HTML-код главной страницы
 $page_content = include_template('main.php', ['categories' => $categories, 'advertisement' => $advertisement]);
@@ -83,5 +62,4 @@ $layout_content = include_template('layout.php', ['main_content' => $page_conten
 
 print($layout_content);
 
-?>
 
