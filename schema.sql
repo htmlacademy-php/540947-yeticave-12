0e14dc09cdd64154619256a3c26134a578cad5dd
Bohdan Dobrov <bov.don.94@gmail.com>
2021-11-01 21:19:00 +0200

    refactor: fixed database - added foreign keys

--- schema.sql
+++ schema.sql
@@ -2,46 +2,49 @@ CREATE DATABASE yeticave13
 DEFAULT CHARACTER SET utf8
 DEFAULT COLLATE utf8_general_ci;
 
-USE yeticave13
+USE yeticave13;
 
 CREATE TABLE category (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(64) NOT NULL UNIQUE,
-    name_category VARCHAR(32) NOT NULL
+    title VARCHAR(255) NOT NULL UNIQUE,
+    title_id VARCHAR(255) NOT NULL UNIQUE
 );
 
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    users_name VARCHAR(255)  NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    passwords VARCHAR(255) NOT NULL,
+    contacts TEXT,
+    dt_add DATETIME DEFAULT CURRENT_TIMESTAMP
+);
 
 CREATE TABLE lot (
     id INT AUTO_INCREMENT PRIMARY KEY,
     users_id INT NOT NULL,
     winner_id INT NOT NULL,
     category_id INT NOT NULL, 
-    dt_add DATETIME(0),
-    name_lot VARCHAR(64) NOT NULL,
+    dt_add DATETIME DEFAULT CURRENT_TIMESTAMP,
+    name_lot VARCHAR(255) NOT NULL,
     description_lot TEXT,
     img_path VARCHAR(255),
     start_price INT NOT NULL,
     date_end DATETIME(0) NOT NULL,
-    bet_step INT
+    bet_step INT,
+    FOREIGN KEY (users_id) REFERENCES users(id),
+    FOREIGN KEY (winner_id) REFERENCES users(id),
+    FOREIGN KEY (category_id) REFERENCES category(id)
 );
 
 CREATE TABLE bet (
     id INT AUTO_INCREMENT PRIMARY KEY,
     users_id INT NOT NULL,
     lot_id INT NOT NULL,
-    dt_add DATETIME(0),
-    final_price INT
+    dt_add DATETIME DEFAULT CURRENT_TIMESTAMP,
+    final_price DECIMAL,
+    FOREIGN KEY (users_id) REFERENCES users(id),
+    FOREIGN KEY (lot_id) REFERENCES lot(id)
 );
 
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    lot_id INT NOT NULL,
-    bet_id INT NOT NULL,
-    users_name VARCHAR(64)  NOT NULL UNIQUE,
-    email VARCHAR(128) NOT NULL UNIQUE,
-    passwords VARCHAR(64) NOT NULL UNIQUE,
-    contacts TEXT,
-    dt_add DATETIME(0)
-);
 
 
