83309a0d37de8661c61563237766942f8bd047d7
Bohdan Dobrov <bov.don.94@gmail.com>
2021-11-15 22:34:37 +0200

    refactor: connected DB to php, results of SQL-queries were connected to the project templates

--- index.php
+++ index.php
@@ -2,51 +2,6 @@
 $is_auth = rand(0, 1);
 
 $user_name = 'Богдан'; // укажите здесь ваше имя
-$categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
-$advertisement = [
-    [
-        'name' => '2014 Rossignol District Snowboard',
-        'category' => 'Доски и лыжи',
-        'price' => 10999,
-        'img_url' => 'img/lot-1.jpg',
-        'end_time' => '2021-10-22',
-    ],
-    [
-        'name' => 'DC Ply Mens 2016/2017 Snowboard',
-        'category' => 'Доски и лыжи',
-        'price' => 15999,
-        'img_url' => 'img/lot-2.jpg',
-        'end_time' => '2021-11-22',
-    ],
-    [
-        'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-        'category' => 'Крепления',
-        'price' => 8000,
-        'img_url' => 'img/lot-3.jpg',
-        'end_time' => '2021-12-13',
-    ],
-    [
-        'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
-        'category' => 'Ботинки',
-        'price' => 10999,
-        'img_url' => 'img/lot-4.jpg',
-        'end_time' => '2021-10-29',
-    ],
-    [
-        'name' => 'Куртка для сноуборда DC Mutiny Charocal',
-        'category' => 'Одежда',
-        'price' => 7500,
-        'img_url' => 'img/lot-5.jpg',
-        'end_time' => '2021-12-01',
-    ],
-    [
-        'name' => 'Маска Oakley Canopy',
-        'category' => 'Разное',
-        'price' => 5400,
-        'img_url' => 'img/lot-6.jpg',
-        'end_time' => '2021-12-31',
-    ]
-];
 
 // Функция показа цены карточки
 function costs_of_item(float $price): string
@@ -71,13 +26,47 @@ function diff_in_time($time_end)
     return [$hours, $minutes];
 };
 
+//Подключаемся к MySql
+$mysqli = new mysqli("127.0.0.1", "root", "", "yeticave13");
+
+//Установка кодировки
+$mysqli->set_charset("utf8mb4");
+
+//Проверка подключения 
+if ($mysqli->connect_error) {
+    error_log('Ошибка при подключении: ' . $mysqli->connect_error);
+}
+
+//Выполнение запроса на показ новых лотов
+$show_new_lot = $mysqli->query("SELECT l.name_lot, l.start_price, l.img_path, MAX(b.final_price) total_price, c.title, l.date_end 
+                            FROM lot l
+                                INNER JOIN bet b ON l.id = b.lot_id
+                                INNER JOIN category c ON l.category_id = c.id
+                            WHERE l.date_end > NOW()
+                            GROUP BY b.lot_id");
+
+//Получение записей в виде ассоциативного массива
+$new_lot = $show_new_lot->fetch_all(MYSQLI_ASSOC);
+
+//Выполнение запроса на показ категорий
+$show_categories = $mysqli->query("SELECT * 
+                                FROM category");
+
+//Получение категорий в виде массива
+$all_categories = $show_categories->fetch_all(MYSQLI_ASSOC);
+
+//Проверка исполнения запроса
+if ($mysqli->connect_error) {
+    error_log('Ошибка при подключении: ' . $mysqli->connect_error);
+}
+
 require_once('helpers.php');
 
 //HTML-код главной страницы
-$page_content = include_template('main.php', ['categories' => $categories, 'advertisement' => $advertisement]);
+$page_content = include_template('main.php', ['categories' => $all_categories, 'advertisement' => $new_lot]);
 
 //HTML-код всей страницы
-$layout_content = include_template('layout.php', ['main_content' => $page_content, 'title' => 'Интернет-аукцион «YetiCave»', 'user_name' => 'Богдан', 'categories' => $categories, 'is_auth' => $is_auth]);
+$layout_content = include_template('layout.php', ['main_content' => $page_content, 'title' => 'Интернет-аукцион «YetiCave»', 'user_name' => 'Богдан', 'categories' => $all_categories, 'is_auth' => $is_auth]);
 
 print($layout_content);
 
